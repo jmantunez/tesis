446cbc582fe55b20aa3e8c4f8886859008be3ae7
Jose <jm.antunez.vinuela@gmail.com>
2015-10-19 20:01:35 -0300

    primer ejemplo led

---
+++ ejemplo led.py
@@ -0,0 +1,23 @@
+__author__ = 'Jose Miguel'
+#!usr/bin/env/ python
+#parpadea.py
+#importamos la libreria GPIO
+import RPi.GPIO as GPIO
+#Importamos la libreria time
+import time
+#Definimos el modo BCM
+GPIO.setmode(GPIO.BCM)
+#Ahora definimos el pin GPIO 17 como salida
+GPIO.setup(17, GPIO.OUT)
+#Queremos que lo que contenga el for i in range se repita 5 veces
+for i in range(0,5):
+        #Asignamos valor logico alto para encenderlo
+               GPIO.output(17, GPIO.HIGH)
+        #Esperamos un segundo
+               time.sleep(1)
+        #Asignamos valor logico bajo para apagarlo
+         GPIO.output(17, GPIO.LOW)
+        #Esperamos un segundo
+         time.sleep(1)
+#Una vez termina las 5 repeticiones, liberamos el pin GPIO utilizado; en este caso el 17
+GPIO.cleanup()
