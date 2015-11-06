efb74b95e5d2a59dcd8262c8a2cce57668b4ac33
Jose <jm.antunez.vinuela@gmail.com>
2015-11-06 00:07:07 -0300

    camara 2

---
+++ preview y captura.py
@@ -0,0 +1,10 @@
+__author__ = 'Jose Miguel'
+import time
+import picamera
+
+with picamera.PiCamera() as camera:
+    camera.resolution = (1024, 768)
+    camera.start_preview()
+    # Camera warm-up time
+    time.sleep(2)
+    camera.capture('foo.jpg')
