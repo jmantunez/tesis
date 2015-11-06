a25e69256617fe1aa7f67321160b20c38f02d10c
Jose <jm.antunez.vinuela@gmail.com>
2015-11-05 23:58:48 -0300

    camara 1

---
+++ preview foto.py
@@ -0,0 +1,11 @@
+__author__ = 'Jose Miguel'
+import time
+import picamera
+
+camera = picamera.PiCamera()
+try:
+    camera.start_preview()
+    time.sleep(10)
+    camera.stop_preview()
+finally:
+    camera.close()
